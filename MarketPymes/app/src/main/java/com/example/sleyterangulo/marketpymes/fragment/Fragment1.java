134729be874d470b036a274d11addcd038c114c4
Sleyter A <sleyterangulo@Sleyters-MacBook-Pro.local>
2016-11-18 13:16:18 -0600

    New commit

--- MarketPymes/app/src/main/java/com/example/sleyterangulo/marketpymes/fragment/Fragment1.java
+++ MarketPymes/app/src/main/java/com/example/sleyterangulo/marketpymes/fragment/Fragment1.java
@@ -6,6 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.sleyterangulo.marketpymes.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class Fragment1 extends Fragment {
 
@@ -18,28 +26,29 @@ public class Fragment1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.frag_home, container, false);
-//        BarChart barChart = (BarChart) rootView.findViewById(R.id.graf1);
-//
-//        ArrayList<BarEntry>batEntries = new ArrayList<>();
-//        batEntries.add(new BarEntry(44f,0));
-//        batEntries.add(new BarEntry(88f,1));
-//        batEntries.add(new BarEntry(66f,2));
-//        batEntries.add(new BarEntry(12f,3));
-//        batEntries.add(new BarEntry(19f,4));
-//        batEntries.add(new BarEntry(91f,5));
-//
-//        BarDataSet barDataSet = new BarDataSet(batEntries,"Dates");
-//
-//        ArrayList<String> theDates = new ArrayList<>();
-//        theDates.add("April");
-//        theDates.add("May");
-//        theDates.add("June");
-//        theDates.add("July");
-//        theDates.add("August");
-//        theDates.add("September");
-//
-//        BarData barData = new BarData(theDates, barDataSet);
-//        barChart.setData(barData);
+        BarChart barChart = (BarChart) rootView.findViewById(R.id.graf1);
+
+        ArrayList<BarEntry> batEntries = new ArrayList<BarEntry>();
+        batEntries.add(new BarEntry(44f,0));
+        batEntries.add(new BarEntry(88f,1));
+        batEntries.add(new BarEntry(66f,2));
+        batEntries.add(new BarEntry(12f,3));
+        batEntries.add(new BarEntry(19f,4));
+        batEntries.add(new BarEntry(91f,5));
+
+        BarDataSet barDataSet;
+
+        barDataSet = new BarDataSet(batEntries,"Dates");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        ArrayList<IBarDataSet> theDates = new ArrayList<IBarDataSet>();
+        theDates.add(barDataSet);
+
+        BarData barData = new BarData(theDates);
+        barData.setValueTextSize(12f);
+        barData.setBarWidth(0.9f);
+        barChart.setTouchEnabled(false);
+        barChart.setData(barData);
 
         return rootView;
     }
