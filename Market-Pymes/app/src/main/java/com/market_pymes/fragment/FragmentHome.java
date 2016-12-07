ce15a01fc2eddc5ad434f63ba627c46b5aa1517c
Luis Serrano <lserrano@yoogooo.com>
2016-12-06 20:01:12 -0600

    codigo para generar garfico pantalla home

--- Market-Pymes/app/src/main/java/com/market_pymes/fragment/FragmentHome.java
+++ Market-Pymes/app/src/main/java/com/market_pymes/fragment/FragmentHome.java
@@ -9,22 +9,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.market_pymes.R;
-import org.apache.http.message.BasicNameValuePair;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import com.market_pymes.Single.Globals;
 import com.market_pymes.helper.InternetStatus;
 import com.market_pymes.helper.JsonHelper;
+
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+
+import android.graphics.Color;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+
 
 public class FragmentHome extends Fragment {
     private TextView Vcont, Vcred;
     private String date, DB_name;
     private InternetStatus IntSts = new InternetStatus();
-    //private PieChart mChart;
-    //private float[] yData = { 5, 10, 15, 30, 40 };
-    //private String[] xData = { "Sony", "Huawei", "LG", "Apple", "Samsung" };
+    PieChart mChart;
+    // we're going to display pie chart for school attendance
+    private int[] yValues = {21, 2, 2};
+    private String[] xValues = {"Present Days", "Absents", "Leaves"};
+
+    // colors for different sections in pieChart
+    public static  final int[] MY_COLORS = {
+            Color.rgb(84,124,101), Color.rgb(64,64,64), Color.rgb(153,19,0),
+            Color.rgb(38,40,53), Color.rgb(215,60,55)
+    };
 
     public FragmentHome() {
     }
@@ -33,8 +59,38 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.frag_home, container, false);
-        ///mChart = (PieChart) rootView.findViewById(R.id.graf1);
-        //BarChart barChart = (BarChart) rootView.findViewById(R.id.graf1);
+
+        //=======================================================================
+        //=======================================================================
+        mChart = (PieChart) rootView.findViewById(R.id.graf);
+
+        //mChart.setUsePercentValues(true);
+//        mChart.setDescription("");
+
+        mChart.setRotationEnabled(true);
+
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+
+            @Override
+            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+                // display msg when value selected
+                if (e == null)
+                    return;
+
+                Toast.makeText(getActivity(),
+                        xValues[e.getXIndex()] + " is " + e.getVal() + "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        // setting sample Data for Pie Chart
+        setDataForPieChart();
+        //=======================================================================
+        //=======================================================================
 
 
         date = getdate();
@@ -51,32 +107,80 @@ public class FragmentHome extends Fragment {
             Toast toast = Toast.makeText(getActivity(), "Se perdió la conexión de datos", Toast.LENGTH_SHORT);
             toast.show();
         }
-
-/*
-        ArrayList<BarEntry> batEntries = new ArrayList<BarEntry>();
-        batEntries.add(new BarEntry(44f,0));
-        batEntries.add(new BarEntry(88f,1));
-        batEntries.add(new BarEntry(66f,2));
-        batEntries.add(new BarEntry(12f,3));
-        batEntries.add(new BarEntry(19f,4));
-        batEntries.add(new BarEntry(91f,5));
-
-        BarDataSet barDataSet;
-
-        barDataSet = new BarDataSet(batEntries,"Dates");
-        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
-        ArrayList<IBarDataSet> theDates = new ArrayList<IBarDataSet>();
-        theDates.add(barDataSet);
-
-        BarData barData = new BarData(theDates);
-        barData.setValueTextSize(12f);
-        barData.setBarWidth(0.9f);
-        barChart.setTouchEnabled(false);
-        barChart.setData(barData);*/
-
         return rootView;
     }
+
+
+    public void setDataForPieChart() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 1; i < yValues.length; i++)
+            yVals1.add(new Entry(yValues[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 1; i < xValues.length; i++)
+            xVals.add(xValues[i]);
+
+        // create pieDataSet
+        PieDataSet dataSet = new PieDataSet(yVals1, "");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        // adding colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        // Added My Own colors
+        for (int c : MY_COLORS)
+            colors.add(c);
+
+
+        dataSet.setColors(colors);
+
+        //  create pie data object and set xValues and yValues and set it to the pieChart
+        PieData data = new PieData(xVals, dataSet);
+        //   data.setValueFormatter(new DefaultValueFormatter());
+        //   data.setValueFormatter(new PercentFormatter());
+
+        data.setValueFormatter(new MyValueFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+
+        // refresh/update pie chart
+        mChart.invalidate();
+
+        // animate piechart
+        mChart.animateXY(1400, 1400);
+
+
+        // Legends to show on bottom of the graph
+        Legend l = mChart.getLegend();
+        l.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+    }
+
+
+    public class MyValueFormatter implements ValueFormatter {
+
+        private DecimalFormat mFormat;
+
+        public MyValueFormatter() {
+            mFormat = new DecimalFormat("###,###,##0"); // use one decimal if needed
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            // write your logic here
+            return mFormat.format(value) + ""; // e.g. append a dollar-sign
+        }
+    }
+
 
     private String getdate() {
         Date d = new Date();
